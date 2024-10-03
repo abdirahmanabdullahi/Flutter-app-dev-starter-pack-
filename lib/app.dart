d064f48581679e3d583b501d5a270ff9990841bc
abdic <Abdiciye2@gmail.com>
2024-10-03 07:24:20 +0300

    First commit

---
+++ lib/app.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/theme/theme.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: TTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      // initialBinding: GeneralBindings(),
+      home: const Scaffold(
+        backgroundColor: TColors.primary,
+        body: Center(
+          child: Text(
+            'Awesome! 🎊 Project Structure is set up and running. \n Happy T Coding 🎊',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
